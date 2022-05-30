58321cfdc63cbc08eead792e8609f6dc3a457ff9
nandu <parmanandpally@gmail.com>
2022-05-30 12:17:05 +0530

    added a new method

--- GitHubCoreConsoleApp/Program.cs
+++ GitHubCoreConsoleApp/Program.cs
@@ -7,6 +7,12 @@ namespace GitHubCoreConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("new line added");
+            Add(12, 22);
+        }
+        static void Add(int num1, int num2)
+        {
+            Console.WriteLine(num1+num2);
         }
     }
 }
